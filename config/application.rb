18385088c9a5f5e6e489402562cfafd13da368a9
Mihail-K <mihail@platterz.ca>
2018-02-21 23:52:13 -0500

    Init database.

--- config/application.rb
+++ config/application.rb
@@ -19,14 +19,14 @@ Bundler.require(*Rails.groups)
 
 module Timekeep
   class Application < Rails::Application
-    # Initialize configuration defaults for originally generated Rails version.
+    config.active_record.schema_format = :sql
+
     config.load_defaults 5.1
 
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration should go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded.
-
-    # Don't generate system test files.
-    config.generators.system_tests = nil
+    config.generators do |g|
+      g.orm                 :active_record, primary_key_type: :uuid
+      g.fixture_replacement :factory_bot
+      g.test_framework      :rspec
+    end
   end
 end

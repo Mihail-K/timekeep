dccee43bd97aa3970a1a3be966be36144966190e
Mihail-K <mihail@platterz.ca>
2018-02-24 01:46:00 -0500

    Accept date parameter, and infer from last event.

--- app/controllers/stats_controller.rb
+++ app/controllers/stats_controller.rb
@@ -1,14 +1,19 @@
 # frozen_string_literal: true
 class StatsController < ApplicationController
   before_action :set_user
+  before_action :set_date
 
   def index
-    @stats = policy_scope(Event).where(date: Date.current, user: @user).longest_duration(limit: 10)
+    @stats = policy_scope(Event).where(date: @date, user: @user).longest_duration(limit: 10)
   end
 
 private
 
   def set_user
     @user = User.find(params[:user_id])
+  end
+
+  def set_date
+    @date = params[:date].presence || policy_scope(Event).where(user: @user).maximum(:date) || Date.current
   end
 end

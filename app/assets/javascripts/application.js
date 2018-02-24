d3918e451ab4effcfdcbe2c134a6ca907a91d03c
Mihail-K <mihail@platterz.ca>
2018-02-24 00:52:21 -0500

    Change inclusion order.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,8 +11,8 @@
 // about supported directives.
 //
 //= require rails-ujs
-//= require turbolinks
 //= require jquery
 //= require highcharts
 //= require chartkick
+//= require turbolinks
 //= require_tree .

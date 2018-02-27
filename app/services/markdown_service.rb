e47e72b69fadaa1f050e29ce03da8d87438f1a7b
Mihail-K <mihail@platterz.ca>
2018-02-27 16:17:16 -0500

    Add service for rendering markdown.

---
+++ app/services/markdown_service.rb
@@ -0,0 +1,33 @@
+# frozen_string_literal: true
+class MarkdownService
+  attr_reader :input
+  attr_reader :options
+
+  def initialize(input, **options)
+    @input   = input
+    @options = default_options.merge(options)
+  end
+
+  def render_html
+    input && html_renderer.render(input)
+  end
+
+  def render_text
+    input && text_renderer.render(input)
+  end
+
+private
+
+  def default_options
+    { filter_html: true }
+  end
+
+  def html_renderer
+    @html_renderer ||= Redcarpet::Markdown.new(Redcarpet::Render::HTML, options)
+  end
+
+  def text_renderer
+    require 'redcarpet/render_strip'
+    @text_renderer ||= Redcarpet::Markdown.new(Redcarpet::Render::StripDown, options)
+  end
+end
